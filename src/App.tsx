719c9a82747beedf4678a3c1b3ab5bc05c429fb0
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-06-23 00:50:49 +0300

    Added TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
