98e196b9752ee99d33932800aa28a02578d316b1
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-06-25 13:18:25 +0300

    Updated card components file structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import Input from './Input';
 import Button from './Button';
-import ProductCard from './ProductCard';
-import CategoryCard from './CategoryCard';
-import ShowCaseProductCard from './ShowCaseProductCard';
 import SearchBar from './SearchBar';
+
+import ProductCard from './card/ProductCard';
+import CategoryCard from './card/CategoryCard';
+import ShowCaseProductCard from './card/ShowCaseProductCard';
 
 import DiscoverPlaceholder from './placeholders/DiscoverPlaceholder';
 import ProductsPlaceholder from './placeholders/ProductsPlaceholder';
