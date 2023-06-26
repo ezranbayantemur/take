5305e732d4515cc1d4821e8ceb4858f3578e1447
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-06-27 00:38:36 +0300

    Fixed product detail image position bug

--- src/pages/ProductDetail/ProductDetail.style.ts
+++ src/pages/ProductDetail/ProductDetail.style.ts
@@ -8,6 +8,7 @@ const IMAGE_HEIGHT = deviceSize.height * 0.45;
 
 export default StyleSheet.create({
   image: {
+    alignSelf: 'center',
     marginTop: padding.SMALL,
     width: IMAGE_WIDTH,
     height: IMAGE_HEIGHT,
