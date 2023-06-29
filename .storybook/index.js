e339be34eed88281d334c09dad9b9b4a6255e495
Ezran Bayantemur <ezranbayantemur@gmail.com>
2023-06-30 02:36:59 +0300

    Fixed Storybook splash screen error

--- .storybook/index.js
+++ .storybook/index.js
@@ -1,7 +1,15 @@
-import { getStorybookUI } from '@storybook/react-native';
+import {getStorybookUI} from '@storybook/react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import './storybook.requires';
+import React from 'react';
 
 const StorybookUIRoot = getStorybookUI({});
 
-export default StorybookUIRoot;
+export default () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return <StorybookUIRoot />;
+};
